78a721d9639c192d543eed26dc426d4ed2664cfb
João Varela <joaovarela@ua.pt>
2025-12-05 00:34:33 +0000

    feat: Add interaction functionality and complete VTK example with cone, sphere, and cylinder

---
+++ VTK/Lesson_01/camera.py
@@ -0,0 +1,74 @@
+###############################################################################
+#       						Cone.py
+###############################################################################
+
+# This example creates a polygonal model of a Cone e visualize the results in a
+# VTK render window.
+# The program creates the cone, rotates it 360º and closes
+# The pipeline  source -> mapper -> actor -> renderer  is typical 
+# and can be found in most VTK programs
+
+# Imports
+
+# Import all VTK modules
+from vtkmodules.all import *
+
+def main():
+
+    coneSource = vtkConeSource()
+    coneSource.SetHeight(2)
+    coneSource.SetRadius(1)
+    coneSource.SetResolution(100)
+    
+    coneMapper = vtkPolyDataMapper()
+    coneMapper.SetInputConnection(coneSource.GetOutputPort())
+  
+    coneActor = vtkActor()
+    coneActor.SetMapper(coneMapper)
+
+
+    # Esfera
+
+    sphereSource = vtkSphereSource()
+    sphereSource.SetRadius(2)
+    sphereSource.SetPhiResolution(100)
+    sphereSource.SetThetaResolution(100)
+
+    sphereMapper = vtkPolyDataMapper()
+    sphereMapper.SetInputConnection(sphereSource.GetOutputPort())
+
+    sphereActor = vtkActor()
+    sphereActor.SetMapper(sphereMapper)
+
+    # Cilindro
+    
+    cylinderSource = vtkCylinderSource()
+    cylinderSource.SetRadius(2)
+    cylinderSource.SetHeight(3)
+    cylinderSource.SetResolution(100)
+
+    cylinderMapper = vtkPolyDataMapper()
+    cylinderMapper.SetInputConnection(cylinderSource.GetOutputPort())
+
+    cylinderActor = vtkActor()
+    cylinderActor.SetMapper(cylinderMapper)
+
+
+    ren = vtkRenderer()
+    ren.SetBackground(1,1,1)
+    # ren.AddActor(coneActor)
+    # ren.AddActor(sphereActor)
+    ren.AddActor(cylinderActor)
+    
+
+    renWin = vtkRenderWindow()
+    renWin.AddRenderer(ren)
+    renWin.SetSize(300,300)  # tamanho original 300x300
+    renWin.SetWindowName('Lesson 1')
+
+    for i in range(0,1360):
+        renWin.Render()
+        ren.GetActiveCamera().Azimuth(1)
+
+if __name__ == '__main__':
+    main()
